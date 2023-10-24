f83143fcf35c1d8298fd395f0eff19cdf1982d19
Yang Chun Ung <qooraven@gmail.com>
2023-10-24 13:53:21 +0900

    Introduce TransactionStageWorker
    - Divide stage, update process
    - Interval configurable

--- PatrolRewardService/PatrolRewardService/TransactionStageWorker.cs
+++ PatrolRewardService/PatrolRewardService/TransactionStageWorker.cs
@@ -6,17 +6,17 @@ using PatrolRewardService.Models;
 
 namespace PatrolRewardService;
 
-public class TransactionWorker : BackgroundService
+public class TransactionStageWorker : BackgroundService
 {
     private readonly IDbContextFactory<RewardDbContext> _contextFactory;
     private readonly NineChroniclesClient _nineChroniclesClient;
     private readonly int _interval;
 
-    public TransactionWorker(NineChroniclesClient client, IDbContextFactory<RewardDbContext> contextFactory,IOptions<WorkerOptions> options)
+    public TransactionStageWorker(NineChroniclesClient client, IDbContextFactory<RewardDbContext> contextFactory, IOptions<WorkerOptions> options)
     {
         _nineChroniclesClient = client;
         _contextFactory = contextFactory;
-        _interval = options.Value.ResultInterval;
+        _interval = options.Value.StageInterval;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -26,21 +26,21 @@ public class TransactionWorker : BackgroundService
             if (stoppingToken.IsCancellationRequested) stoppingToken.ThrowIfCancellationRequested();
 
             var dbContext = await _contextFactory.CreateDbContextAsync(stoppingToken);
-            await UpdateTx(dbContext, _nineChroniclesClient, stoppingToken);
+            await StageTx(dbContext, _nineChroniclesClient, stoppingToken);
             await Task.Delay(_interval, stoppingToken);
         }
     }
 
-    public static async Task UpdateTx(RewardDbContext dbContext, NineChroniclesClient client,
+    public static async Task StageTx(RewardDbContext dbContext, NineChroniclesClient client,
         CancellationToken stoppingToken)
     {
         var transactions = dbContext.Transactions
-            .Where(p => p.Result == TransactionStatus.STAGING || p.Result == TransactionStatus.INVALID).ToList();
-        foreach (var tx in transactions)
+            .Where(p => p.Result == TransactionStatus.CREATED).ToList();
+        foreach (var transaction in transactions)
         {
-            var result = await client.Result(tx.TxId);
-            tx.Result = result.txStatus;
-            tx.ExceptionName = result.exceptionName;
+            var tx = Transaction.Deserialize(Convert.FromBase64String(transaction.Payload));
+            await client.StageTx(tx);
+            transaction.Result = TransactionStatus.STAGING;
         }
 
         dbContext.UpdateRange(transactions);
